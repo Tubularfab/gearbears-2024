7d6615d13b7b3a20e7f1be216f8ff1a087c5dc86
Greg Cruce VM <greg@vennmarketing.com>
2024-03-08 21:48:51 -0500

    cleaning up indentation

--- main/java/frc/robot/RobotContainer.java
+++ main/java/frc/robot/RobotContainer.java
@@ -40,117 +40,118 @@ public class RobotContainer {
   //PWMSparkMax shooterRight = new PWMSparkMax(42);
   //PWMSparkMax shooterLeft = new PWMSparkMax(41);
 
-  
-  
 
-  SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-  private final Shooter m_shooter = new Shooter();
-  private final Intake m_intake = new Intake();
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final Joystick driver = new Joystick(0);
 
-  // Bindings
-  Trigger buttonOne = new JoystickButton(driver, 1);
-  Trigger buttonNine = new JoystickButton(driver, 9);
-  Trigger buttonTen = new JoystickButton(driver, 10);
-  Trigger buttonEleven = new JoystickButton(driver, 11); 
-  Trigger buttonTwelve = new JoystickButton(driver, 12);
+    SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+    private final Shooter m_shooter = new Shooter();
+    private final Intake m_intake = new Intake();
+    
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final Joystick driver = new Joystick(0);
+
+    // Bindings
+    Trigger buttonOne = new JoystickButton(driver, 1);
+    Trigger buttonNine = new JoystickButton(driver, 9);
+    Trigger buttonTen = new JoystickButton(driver, 10);
+    Trigger buttonEleven = new JoystickButton(driver, 11); 
+    Trigger buttonTwelve = new JoystickButton(driver, 12);
 
 
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-      public RobotContainer() {
-          // Configure the trigger bindings
-
+    public RobotContainer() {
+        
+        // Configure the trigger bindings
         swerveSubsystem.setDefaultCommand(new RunCommand(()-> swerveSubsystem.drive(driver.getX(), (-driver.getY()), (-driver.getZ())), swerveSubsystem));
         
-        
-        
         configureBindings();
-      } // end RobotContainer
-
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private Command turnInTakeOn() {
     
-    return null;
-    }
+    } // end RobotContainer
+
+    /**
+    * Use this method to define your trigger->command mappings. Triggers can be created via the
+    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+    * predicate, or via the named factories in {@link
+    * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+    * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+    * joysticks}.
+    */
+    private Command turnInTakeOn() {
     
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //new JoystickButton(driver, 1).whileTrue(m_shooter.setShooterSpeed(0.5));
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
+        return null;
 
-    buttonOne.whileTrue(m_shooter.getShooterCommand());
-    buttonTen.whileTrue(new SequentialCommandGroup(
-      m_shooter.getStartShooterCommand(),
-      new WaitCommand(1.5),
-      m_intake.getRunIntakeCommand()
+    } // end turnIntakeOn
+    
+    private void configureBindings() {
+        
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        // new JoystickButton(driver, 1).whileTrue(m_shooter.setShooterSpeed(0.5));
+        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+        // cancelling on release.
 
-    ).handleInterrupt(() -> {m_shooter.setShooterSpeed(0);}));
-    buttonEleven.whileTrue(m_intake.getRunIntakeCommand());
-    buttonNine.whileTrue(m_shooter.getSlowShootCommand());
-    buttonTwelve.whileTrue(new ParallelCommandGroup(
-      m_shooter.getRunReverseShooter(),
-      m_intake.getRunOutakeCommand()));
+        buttonOne.whileTrue(m_shooter.getShooterCommand());
+        buttonTen.whileTrue(new SequentialCommandGroup(
+            m_shooter.getStartShooterCommand(),
+            new WaitCommand(1.5),
+            m_intake.getRunIntakeCommand()
+        ).handleInterrupt(
+            () -> { m_shooter.setShooterSpeed(0); }
+        ));
+        buttonEleven.whileTrue(m_intake.getRunIntakeCommand());
+        buttonNine.whileTrue(m_shooter.getSlowShootCommand());
+        buttonTwelve.whileTrue(new ParallelCommandGroup(
+        m_shooter.getRunReverseShooter(),
+        m_intake.getRunOutakeCommand()));
 
-  }
+    } // end configureBindings
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  //private void getAutonomousCommand() {
+    /**
+    * Use this to pass the autonomous command to the main {@link Robot} class.
+    *
+    * @return the command to run in autonomous
+    */
+    //private void getAutonomousCommand() {
     public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+        // An example command will be run in autonomous
 
-      // return new SequentialCommandGroup(
-      //   new ParallelCommandGroup(
-      //   m_shooter.getShooterCommand(),
-      //   m_intake.getRunIntakeCommand()),
+        // return new SequentialCommandGroup(
+        //   new ParallelCommandGroup(
+        //   m_shooter.getShooterCommand(),
+        //   m_intake.getRunIntakeCommand()),
 
-      //   new WaitCommand(1),
+        //   new WaitCommand(1),
 
-      //   new ParallelCommandGroup(m_shooter.getStopCommand(), m_intake.getStopCommand())
-      // );
+        //   new ParallelCommandGroup(m_shooter.getStopCommand(), m_intake.getStopCommand())
+        // );
 
-            return new SequentialCommandGroup(
-                m_shooter.getStartShooterCommand(),
-                new WaitCommand(.50),
+        return new SequentialCommandGroup(
+            m_shooter.getStartShooterCommand(),
+            new WaitCommand(.50),
 
-                m_intake.getRunIntakeCommand().withTimeout(.5),
-                m_shooter.getStopCommand(),
-                new ParallelCommandGroup(
-                  // swerveSubsystem.getDriveStraightCommand(.3).withTimeout(2.75), 
-                  // m_intake.getRunIntakeCommand().withTimeout(2.75),
-                  swerveSubsystem.getDriveStraightCommand(.3), 
-                  m_intake.getRunIntakeCommand()
-                ).withTimeout(1.5),
-                swerveSubsystem.getDriveStraightCommand(-.3).withTimeout(1.5)
-                // new WaitCommand(.50),
-                // m_shooter.getStartShooterCommand()
+            m_intake.getRunIntakeCommand().withTimeout(.5),
+            m_shooter.getStopCommand(),
+            new ParallelCommandGroup(
+            
+                // swerveSubsystem.getDriveStraightCommand(.3).withTimeout(2.75), 
+                // m_intake.getRunIntakeCommand().withTimeout(2.75),
+                
+                swerveSubsystem.getDriveStraightCommand(.3), 
+                m_intake.getRunIntakeCommand()
+            
+            ).withTimeout(1.5),
+            swerveSubsystem.getDriveStraightCommand(-.3).withTimeout(1.5)
+            // new WaitCommand(.50),
+            // m_shooter.getStartShooterCommand()
 
-            );
+        );
         
         // new swerveSubsystem.setDefaultCommand(new RunCommand(()-> swerveSubsystem.drive(driver.getX(), (-driver.getY(3)), (-driver.getZ())), swerveSubsystem));
 
-      // pathplanner - tool to review math & trajectories
+        // pathplanner - tool to review math & trajectories
 
   } // end getAutonomousCommand
 
-   
-   
-    
-  
 
 } // end class
